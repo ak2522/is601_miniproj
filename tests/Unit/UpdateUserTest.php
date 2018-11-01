b4ed4a67b23eb8d831090c96501e7575006b8a2e
anupama <ak2522@njit.edu>
2018-11-01 19:51:13 -0400

    Task: Create a unit test to update a user into the users table

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateUser()
+
+    {
+        $user = User::find(5);
+        $user->name = 'Steve Smith';
+        $user->save();
+        $this->assertDatabaseHas('users', ["name" => "Steve Smith"]);
+    }
+
+}
