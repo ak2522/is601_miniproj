201c0bd1f16f4632d5217671386e6a5b05e7b52a
anupama <ak2522@njit.edu>
2018-11-01 15:22:38 -0400

    Feature: Created 3 properties make,model,year in CarFactory

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Car::class, function (Faker $faker) {
+    return [
+        //'make' => $faker->firstName(),
+        'make' => $faker->randomElement(['firstName[]'=>'Ford', 'Honda', 'Toyota']),
+        'model' => $faker->company(),
+        'year' => $faker->date(),
+    ];
+});
